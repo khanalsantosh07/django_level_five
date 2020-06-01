9e3823cac0ececed5340265e8790ebf0d73acf08
Santosh khanal <connect.santoshkhanal@gmail.com>
2020-06-01 23:32:05 +0545

    First commit

--- learning_users/basic_app/urls.py
+++ learning_users/basic_app/urls.py
@@ -5,6 +5,7 @@ from basic_app import views
 app_name = 'basic_app'
 
 urlpatterns = [
+
     path('register/',views.register,name='register'),
     path('user_login/',views.user_login,name='user_login')
 ]
